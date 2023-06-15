ebe7b08145b34301463f902d4613d91c41ef9b8b
sanjana8100 <sanjanams812@gmail.com>
2023-06-15 21:49:01 +0530

    added: P4

---
+++ Loops.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND7_CSharpProgrammingExercises
+{
+    internal class Loops
+    {
+        public static void PrimeNumbersRange(int range)
+        {
+            Console.WriteLine($"The Prime Numbers Between the range 2 to {range} are:");
+            for (int number = 2; number <= range; number++)
+            {
+                int count = 0;
+                for (int index = 2; index <= number/2; index++)
+                {
+                    if(number % index == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0 && number != 1)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,17 +4,27 @@
     {
         static void Main(string[] args)
         {
+            // Basics:
+
             //Console.WriteLine("P1- Given two integers, write a method that swaps them using temporary variable");
             //Console.WriteLine("----------------------------------------------------------------------------------");
             //Basic.SwapTwoNumbers(10, 20);
+            
+            // Conditional Statements:
 
             //Console.WriteLine("P2- Given a year as integer, write a method that checks if year is leap year");
             //Console.WriteLine("----------------------------------------------------------------------------------");
             //Console.WriteLine(ConditionalStatements.LeapYear(2022));
 
-            Console.WriteLine("P3- Write a method that checks if given number (positive integer) contains digit 3");
+            //Console.WriteLine("P3- Write a method that checks if given number (positive integer) contains digit 3");
+            //Console.WriteLine("----------------------------------------------------------------------------------");
+            //Console.WriteLine(ConditionalStatements.Contains3(874365));
+
+            // Loops:
+
+            Console.WriteLine("P4- Given an integer n (n>2), write a method that returns prime numbers from range [2, n]");
             Console.WriteLine("----------------------------------------------------------------------------------");
-            Console.WriteLine(ConditionalStatements.Contains3(874365));
+            Loops.PrimeNumbersRange(30);
         }
     }
 }
