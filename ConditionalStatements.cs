3d2a2d4d7cf01f5274230910f32d2a67154eaae3
sanjana8100 <sanjanams812@gmail.com>
2023-06-15 19:31:13 +0530

    added: P2

---
+++ ConditionalStatements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND7_CSharpProgrammingExercises
+{
+    internal class ConditionalStatements
+    {
+        public static bool LeapYear(int year)
+        {
+            Console.WriteLine($"Is {year} a Leap Year? ");
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("P1- Given two integers, write a method that swaps them using temporary variable");
+            //Console.WriteLine("P1- Given two integers, write a method that swaps them using temporary variable");
+            //Console.WriteLine("----------------------------------------------------------------------------------");
+            //Basic.SwapTwoNumbers(10, 20);
+
+            Console.WriteLine("P2- Given a year as integer, write a method that checks if year is leap year");
             Console.WriteLine("----------------------------------------------------------------------------------");
-            Basic.SwapTwoNumbers(10, 20);
+            Console.WriteLine(ConditionalStatements.LeapYear(2022));
         }
     }
 }
