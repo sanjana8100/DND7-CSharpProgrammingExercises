d6b894f1dfe2c8714174a993021f147541e4b3ec
sanjana8100 <sanjanams812@gmail.com>
2023-06-15 19:00:23 +0530

    added: C Sharp Programming Exercises

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DND7_CSharpProgrammingExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
